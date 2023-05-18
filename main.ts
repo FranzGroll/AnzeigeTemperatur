e1c4b42f711fa6e57d371e3b043de1e026e82418
FranzGroll <133787328+FranzGroll@users.noreply.github.com>
2023-05-18 14:01:15 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,30 @@
+
+
+
+
+let temperature = 0
+let tempstr = ""
+lcd1602.setAddress(
+lcd1602.I2C_ADDR.addr1
+)
+lcd1602.putString(
+"Temperatur =",
+3,
+0
+)
+lcd1602.set_LCD_Show(lcd1602.visibled.visible)
+lcd1602.set_backlight(lcd1602.on_off.on)
+basic.forever(function () {
+    temperature = pins.analogReadPin(AnalogPin.P0) / 2.1
+    temperature = Math.round(temperature)
+    temperature /= 10
+lcd1602.putNumber(
+    temperature,
+    3,
+    1
+    )
+    basic.pause(1000)
+    if (temperature > 23) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+})
